de19f088d0f9af556730850930e7ed8a82e351ed
INFINITY <1374122390@qq.com>
2026-03-03 17:52:55 +0800

    Fixing PGmap

--- DBG/Assets/Script/PG_Map.cs
+++ DBG/Assets/Script/PG_Map.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PG_Map : MonoBehaviour
 {
-    
     [Header("Map Settings")]
     [SerializeField] private int numLevel = 15;
     [SerializeField] private int maxShop = 3;
+    [SerializeField] private int maxWidth = 6;
 
-    private List<List<Node>> map = new List<List<Node>>();
+    public List<List<Node>> map = new List<List<Node>>();
     private int shopCount = 0;
 
     public enum RoomType
@@ -24,6 +24,9 @@ public class PG_Map : MonoBehaviour
     {
         public int layer;
         public RoomType type;
+
+        public List<Node> nextNodes = new List<Node>();
+        public List<Node> prevNodes = new List<Node>();
 
         public Node(int l, RoomType t)
         {
@@ -52,45 +55,66 @@ public class PG_Map : MonoBehaviour
             List<Node> prevLayer = map[level - 1];
             List<Node> newLayer = new List<Node>();
 
-            bool isExpansionPhase = level < numLevel / 2;
+            bool isExpansion = level < numLevel / 2;
 
-            int targetCount = prevLayer.Count;
-
-            // -------- 扩张阶段 --------
-            if (isExpansionPhase)
+            // ---------- 最后一层 Boss ----------
+            if (level == numLevel - 1)
             {
-                if (level == 1)
-                {
-                    targetCount = 2; // 强制分叉
-                }
-                else
-                {
-                    if (Random.value > 0.5f)
-                        targetCount += 1;
+                Node boss = new Node(level, RoomType.Boss);
+                newLayer.Add(boss);
 
-                    if (targetCount < 2)
-                        targetCount = 2;
+                foreach (var node in prevLayer)
+                {
+                    node.nextNodes.Add(boss);
+                    boss.prevNodes.Add(node);
+                }
+
+                map.Add(newLayer);
+                continue;
+            }
+
+            // ---------- 扩张阶段 ----------
+            if (isExpansion)
+            {
+                foreach (var node in prevLayer)
+                {
+                    // 第一层强制分叉
+                    int branchCount = (level == 1) ? 2 :
+                        (Random.value > 0.5f ? 2 : 1);
+
+                    for (int i = 0; i < branchCount; i++)
+                    {
+                        if (newLayer.Count >= maxWidth)
+                            break;
+
+                        Node child = new Node(level, RollRoomType(level));
+
+                        node.nextNodes.Add(child);
+                        child.prevNodes.Add(node);
+
+                        newLayer.Add(child);
+                    }
                 }
             }
-            // -------- 收缩阶段 --------
+            // ---------- 收缩阶段 ----------
             else
             {
-                float shrinkChance = (float)(level - numLevel / 2) / (numLevel / 2);
+                for (int i = 0; i < prevLayer.Count; i += 2)
+                {
+                    Node merged = new Node(level, RollRoomType(level));
+                    newLayer.Add(merged);
 
-                if (Random.value < shrinkChance)
-                    targetCount -= 1;
+                    // 当前节点连接
+                    prevLayer[i].nextNodes.Add(merged);
+                    merged.prevNodes.Add(prevLayer[i]);
 
-                if (level == numLevel - 1)
-                    targetCount = 1; // 最后一层Boss
-            }
-
-            targetCount = Mathf.Clamp(targetCount, 1, 6);
-
-            // 生成节点
-            for (int i = 0; i < targetCount; i++)
-            {
-                RoomType type = RollRoomType(level);
-                newLayer.Add(new Node(level, type));
+                    // 相邻节点合并
+                    if (i + 1 < prevLayer.Count)
+                    {
+                        prevLayer[i + 1].nextNodes.Add(merged);
+                        merged.prevNodes.Add(prevLayer[i + 1]);
+                    }
+                }
             }
 
             map.Add(newLayer);
@@ -101,9 +125,6 @@ public class PG_Map : MonoBehaviour
 
     RoomType RollRoomType(int level)
     {
-        if (level == numLevel - 1)
-            return RoomType.Boss;
-
         float shopChance = 0.1f;
         float specialChance = 0.15f;
 
@@ -151,7 +172,7 @@ public class PG_Map : MonoBehaviour
 
             foreach (var node in map[i])
             {
-                line += node.type.ToString() + " ";
+                line += node.type + "(" + node.nextNodes.Count + ") ";
             }
 
             Debug.Log(line);
